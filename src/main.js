624504bf6f56d28314424d2a403ba2cf39a10b24
Christ <mattlabatte99@gmail.com>
2025-10-31 16:57:02 -0400

    Barre de navigation

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 import { gsap } from "gsap";
 
+const menuIcon = document.querySelector('.menu-icon');
+const menuNav = document.querySelector('header nav');
+
+function ouvreFermeMenu() {
+  menuNav.classList.toggle('closed');
+}
+
+menuIcon.addEventListener('click', ouvreFermeMenu);
+
 
 import './style.css'
