590ff50d4190bc1765d5d5f8061edddb930f33c3
Edgar <2252988@bdeb.qc.ca>
2025-11-13 16:11:55 -0500

    Ajustement

--- src/main.js
+++ src/main.js
@@ -27,17 +27,31 @@ const swiper = new Swiper(".swiper", {
   },
 });
 
+const swiperProduit = new Swiper(".produit", {
+  loop: true,
+  spaceBetween: 30,
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 const boutonCote = document.getElementById("boutonCote");
 boutonCote.addEventListener("click", () => {
-  swiper.slideToLoop(0);
+  swiperProduit.slideToLoop(0); 
 });
 
 const boutonDessus = document.getElementById("boutonDessus");
 boutonDessus.addEventListener("click", () => {
-  swiper.slideToLoop(1);
+  swiperProduit.slideToLoop(1); 
 });
 
 const boutonDos = document.getElementById("boutonDos");
 boutonDos.addEventListener("click", () => {
-  swiper.slideToLoop(2);
+  swiperProduit.slideToLoop(2); 
 });
