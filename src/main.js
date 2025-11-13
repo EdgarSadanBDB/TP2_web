759f5705012622871287ec2ec074c48367ce0e39
Edgar <2252988@bdeb.qc.ca>
2025-11-13 08:07:03 -0500

    Ajout Slider Page Produit

--- src/main.js
+++ src/main.js
@@ -13,3 +13,38 @@ menuIcon.addEventListener('click', ouvreFermeMenu);
 
 
 import './style.css'
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  spaceBetween: 30,
+  effect: "fade",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const boutonCote = document.getElementById("boutonCote");
+boutonCote.addEventListener("click", () => {
+  swiper.slideToLoop(0); 
+});
+
+const boutonDessus = document.getElementById("boutonDessus");
+boutonDessus.addEventListener("click", () => {
+  swiper.slideToLoop(1); 
+});
+
+const boutonDos = document.getElementById("boutonDos");
+boutonDos.addEventListener("click", () => {
+  swiper.slideToLoop(2); 
+});
+
+
+
+
+
+
